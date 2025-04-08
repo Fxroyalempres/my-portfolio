f9d76848d1292cafcf315a83258533735a87b95b
Voice of Reason #FxRoyalempress <doreenjoki98@gmail.com>
2025-04-08 14:41:58 +0300

    Update script.js
    updated script

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 successMessage.classList.add("show");
+
+clicksound.volume = 0.5; // 50% volume
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
